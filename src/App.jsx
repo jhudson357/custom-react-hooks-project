0d8c6d8a18b33834a80d266240363b6b757590fe
James Hudson <jmh357@comcast.net>
2023-03-02 12:09:01 -0600

    update firebase database link

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     setError(null)
     try {
       const response = await fetch(
-        'https://react-http-790d2-default-rtdb.firebaseio.com//tasks.json'
+        'https://react-http-9ce9a-default-rtdb.firebaseio.com/tasks.json'
       )
 
       if (!response.ok) {
